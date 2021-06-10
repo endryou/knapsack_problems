08ec93aa9ba18190f1da80fe4b09756bf920454b
Andrzej Sołoducha <soloducha.andrzej@gmail.com>
2021-06-11 00:59:19 +0200

    tests.sh updated

--- tests/test.sh
+++ tests/test.sh
@@ -4,19 +4,20 @@ PROJECT_ROOT=$( dirname $(dirname $(realpath $0)))
 #for i in $(seq 1 30);
 #  do  $PROJECT_ROOT/bin/run UKP greedy $PROJECT_ROOT/datasets/ukp/01 --capacity=120
 #done
-
+CAPACITY=10000000
+DATASET=100_01.txt
 #UKP
-$PROJECT_ROOT/bin/run UKP greedy $PROJECT_ROOT/datasets/ukp/01 --capacity=120
-$PROJECT_ROOT/bin/run UKP bruteForce $PROJECT_ROOT/datasets/ukp/01 --capacity=120
-$PROJECT_ROOT/bin/run UKP dynamic $PROJECT_ROOT/datasets/ukp/01 --capacity=120
+$PROJECT_ROOT/bin/run UKP greedy $PROJECT_ROOT/datasets/instances/$DATASET --capacity=$CAPACITY -v
+$PROJECT_ROOT/bin/run UKP bruteForce $PROJECT_ROOT/datasets/instances/$DATASET --capacity=$CAPACITY
+$PROJECT_ROOT/bin/run UKP dynamic $PROJECT_ROOT/datasets/instances/$DATASET --capacity=$CAPACITY
 
 #MKP
-$PROJECT_ROOT/bin/run MKP boundAndBound $PROJECT_ROOT/datasets/mkp/01/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/01/knapsacks --backtracks=10
-$PROJECT_ROOT/bin/run MKP greedy $PROJECT_ROOT/datasets/mkp/01/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/01/knapsacks
+#$PROJECT_ROOT/bin/run MKP boundAndBound $PROJECT_ROOT/datasets/mkp/01/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/01/knapsacks --backtracks=30
+#$PROJECT_ROOT/bin/run MKP greedy $PROJECT_ROOT/datasets/mkp/01/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/01/knapsacks
 
 #MKP
-$PROJECT_ROOT/bin/run MKP boundAndBound $PROJECT_ROOT/datasets/mkp/02/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/02/knapsacks --backtracks=10
-$PROJECT_ROOT/bin/run MKP greedy $PROJECT_ROOT/datasets/mkp/02/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/02/knapsacks
+#$PROJECT_ROOT/bin/run MKP boundAndBound $PROJECT_ROOT/datasets/mkp/02/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/02/knapsacks --backtracks=30
+#$PROJECT_ROOT/bin/run MKP greedy $PROJECT_ROOT/datasets/mkp/02/items --knapsacksPath=$PROJECT_ROOT/datasets/mkp/02/knapsacks
 
 
 
